bb54bc4f241412ea051447fbc6b4a0746701f0d0
Juan Gasca <juan.gasca@Seguritech.local>
2019-05-24 17:53:09 -0500

    Examen de Java

---
+++ demo/src/main/java/com/prueba_1/demo/person.java
@@ -0,0 +1,7 @@
+package com.prueba_1.demo;
+
+public class person {
+
+    private String nombre;
+    private String apellido;
+}
---
+++ demo/src/main/java/com/prueba_1/demo/training.java
@@ -0,0 +1,14 @@
+package com.prueba_1.demo;
+
+public class training {
+
+    public static void cantidaddeinscritos (){
+        return cantidaddeinscritos = null();
+
+    }
+
+    private String nombredecurso;
+    private String listadepersonasinscritas;
+}
+
+}
